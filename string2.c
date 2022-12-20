434edc6decf0b80c687d8aa8008b1c258a5610bf
Festus Omaku <fomaku3@gmail.com>
2022-12-20 14:45:29 +0100

    err

--- string2.c
+++ string2.c
@@ -64,7 +64,7 @@ void _puts(char *str)
  *_putchar - writes the character to stdout
  *@c: The character to print
  *Return: On success 1.
- *	On error, -1 is returned, and errno is set appropriately.
+ *	On error, -1 is returned, and errno is set
  */
 int _putchar(char c)
 {
